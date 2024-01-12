40f5cfd3e5e6bc79cfb3eef947f4db3c8415a3a2
Aymane <aymane.naouadir18@gmail.com>
2024-01-12 21:01:41 +0100

    create a function sh_command, and move the core code to it

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,12 @@ use std::env;
 use std::process::Command;
 
 fn main() {
-
     let args: Vec<String> = env::args().collect();
+
+    sh_command(&args);
+}
+
+fn sh_command(args: &Vec<String>) {
     let program = Command::new(&args[1])
             .args(args.clone().drain(2..))
             .spawn();
