ac72a09f3abd4dafa36a1074b5384d7951e49b7a
Aymane <aymane.naouadir18@gmail.com>
2024-01-12 17:00:35 +0100

    creating a child process and passing one static arg

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 use std::env;
+use std::process::Command;
 
 fn main() {
 
-    let args = env::args();
+    let args: Vec<String> = env::args().collect();
+    let program = Command::new(&args[1])
+            .arg("rust is great")
+            .spawn();
 
-    println!("Number of arguments: {}", args.len());
-    for arg in args {
-        println!("{}", arg);
+    match program {
+        Ok(mut child) => {
+            child.wait().expect("failed to wait the child process");
+            println!("succefully created the spawned process");
+        }
+        Err(err) => {
+            println!("error: {}", err);
+        }
     }
 }
